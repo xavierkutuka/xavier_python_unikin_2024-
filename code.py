964bd56e1e8c3315cc3a603ee3b997e446c24ca4
Franklin <franklinmwamba67@gmail.com>
2024-03-24 00:14:59 +0100

    calcul valeurs et vecteurs propres matrice carrée d'ordre 10 rapport de travail

---
+++ code.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+#  matrice carrée d'ordre 10
+A = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+              [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+              [3, 6, 9, 12, 15, 18, 21, 24, 27, 30],
+              [4, 8, 12, 16, 20, 24, 28, 32, 36, 40],
+              [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+              [6, 12, 18, 24, 30, 36, 42, 48, 54, 60],
+              [7, 14, 21, 28, 35, 42, 49, 56, 63, 70],
+              [8, 16, 24, 32, 40, 48, 56, 64, 72, 80],
+              [9, 18, 27, 36, 45, 54, 63, 72, 81, 90],
+              [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]])
+
+# Calculer les valeurs propres et les vecteurs propres
+eig_vals, eig_vecs = np.linalg.eig(A)
+
+# Afficher les valeurs propres
+print("Valeurs propres :")
+print(eig_vals)
+print()
+
+# Afficher les vecteurs propres
+print("Vecteurs propres :")
+print(eig_vecs)
